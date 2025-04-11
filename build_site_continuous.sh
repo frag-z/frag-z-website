4e99ca54eff142e08b9d15c93601f36ed6caa7e6
ccn <ccn@cuppajoeman.com>
2025-04-11 17:49:36 -0400

    feat: add skills page

---
+++ build_site_continuous.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 scripts/fast-html/main.py -d --base-dir html/ --gen-dir generated_html --base-template-file scripts/frag_z_template.html --custom-template-conversion-file scripts/template_conversion.py 
